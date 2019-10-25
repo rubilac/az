4228e9e2eb4009fe3baabc5115ac9ea76bcbdbd4
Mark Trent <mark.trent@snapt-ui.com>
2019-10-25 10:00:22 +0200

    kill and runner

---
+++ src/runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo `date` + "Starting Runner.sh" >> runs.log
+
+for (( ; ; ))
+do
+   #sudo rm -rf /var/log/kern.log.*
+   echo `date` >> runs.log
+   python3 az_combat.py
+   python3 az_farmer.py
+   python3 az_crafting.py
+   python3 az_gather.py
+   python3 az_inv.py
+done
