1934aebcd578090e9f7c590ba1754ed938fbc49b
mark <mark.trent86@gmail.com>
2019-10-25 12:10:17 -0700

    Equipment

--- src/az_brawl.py
+++ src/az_brawl.py
@@ -30,7 +30,11 @@ class Brawl():
 		self.skip_pos = (775, 705)
 		self.close_pos = (1123, 332)
 		self.close_item_pos = (988, 397)
+		self.close_gear_pos = (984, 403)
 		self.remove_shield = (849, 596)
+		self.new_enemy = (1090, 471)
+		self.left_arrow = (569, 520)
+		self.right_arrow = (984, 522)
 		# Brawlers
 		self.worker = (562, 586)
 		self.edifis = (645, 584)
@@ -113,7 +117,7 @@ class Brawl():
 			fres = np.where(result >= 0.95)
 			if fres[0].size == 0:
 				print("Did not find Coal. Using first position")
-				move_and_click((653, 460), 1)
+				move_and_click((906, 467), 1)
 				return False
 			elif fres[0].size > 1:
 				cord = (fres[1][0]+x, fres[0][0]+y)
@@ -128,20 +132,78 @@ class Brawl():
 			move_and_click(self.close_pos, 1)		
 
 
+	def equip_gear(self, retry=0):
+		x, y, w, h = self.brawl_item_screen
+		self.get_image(x, y, w, h, 'brawl_gear')
+		img = cv2.imread('../templates/brawl/obelisk.png')
+		screen = cv2.imread('tmp_brawl_gear.png')
+		try:
+			result = cv2.matchTemplate(screen, img, method) # Does it match?
+			fres = np.where(result >= 0.95)
+			if fres[0].size == 0 and retry == 0:
+				print("Did not find obelisk. checking next page...")
+				move_and_click(self.left_arrow, 1)
+				move_and_click(self.left_arrow, 1)
+				move_and_click(self.right_arrow, 1)
+				self.equip_gear(1)
+			elif fres[0].size > 1:
+				cord = (fres[1][0]+x, fres[0][0]+y)
+				print("Found Obelisk @ {}, {}. Equipping Obelisk!".format(cord[0], cord[1]))
+				move_and_click(cord, 1)
+				return True
+			elif fres[0].size == 1:
+				cord = (fres[1]+x, fres[0]+y)
+				print("Found Obelisk @ {}, {}. Equipping Obelisk!".format(cord[0], cord[1]))
+				move_and_click(cord, 1)
+				return True
+		except:
+			print("Did not find item. Going ahead unequipped!")
+			move_and_click(self.close_gear_pos, 1)
+
+
+	def gear_check(self):
+		x, y, w, h = b.brawl_item_screen #(580, 390, 970, 670)
+		mousePos(self.new_enemy,1)
+		self.get_image()
+		screen = cv2.imread('tmp_brawl.png')
+		img = cv2.imread('../templates/brawl/empty_weapon.png')
+		try:
+			result = cv2.matchTemplate(screen, img, method) # Does it match?
+			fres = np.where(result >= 0.95)
+			if fres[0].size == 0:
+				print("Everyone is Equipped, battle away!")
+				return True
+			elif fres[0].size > 1:
+				cord = (fres[1][0]+self.brawl_screen[0], fres[0][0]+self.brawl_screen[1]) #(400, 300, 1150, 730)
+				print("M-Found empty weapon @ {}, {}. Equipping Obelisk".format(cord[0], cord[1]))
+				move_and_click(cord, 1)
+				self.equip_gear()
+				self.gear_check()
+			elif fres[0].size == 1:
+				cord = (fres[1]+self.brawl_screen[0], fres[0]+self.brawl_screen[1])
+				print("S-Found empty weapon @ {}, {}. Equipping Obelisk.".format(cord[0], cord[1]))
+				move_and_click(cord, 1)
+				self.equip_gear()
+				self.gear_check()
+		except:
+			print("Issue with checking empty weapon")
+			move_and_click(self.close_pos, 1)		
+
 
 	def squad_fight(self, squad_name):
 		for brawler in squad_name: # Click the brawlers
 			move_and_click(brawler, 2)
-		move_and_click(self.place_bet_pos, 1) # Click Bet Button
+		self.gear_check()
+		move_and_click(self.place_bet_pos, 1) # Click Bet Button		
 		get_coal = self.find_item()
 		if get_coal == False:
 			pass
 		else:
-			move_and_click(get_coal, 1) # Get and Click Coal
+			move_and_click(get_coal, 1)
 		move_and_click(self.first_pot_pos, 1) # Open first potion box
 		move_and_click(self.first_pot_pos, 1) # Select first potion in list
 		move_and_click(self.close_item_pos, 1) # Cursory potion box close in case no potions
-		move_and_click((1090, 471), 1)
+		move_and_click(self.new_enemy, 1)
 		move_and_click(self.fight_pos, 1) # Fight
 		move_and_click(self.remove_shield, 1) # Remove shield
 		move_and_click(self.skip_pos, 1) # skip
@@ -165,10 +227,12 @@ class Brawl():
 	def test_fight(self):
 		refresh_checker()
 		self.open_ring()
-		self.squad_fight(self.squad_loser)
+		self.squad_fight(self.squad_1)
 		move_and_click(self.close_pos, 1)
 
 
 if __name__ == '__main__':
 	b = Brawl()
 	b.org_fight()
+	#x, y, w, h = b.brawl_item_screen
+	#b.get_image(x, y, w, h, 'brawl_gear')	
