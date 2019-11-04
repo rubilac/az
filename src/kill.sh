8f4fa79a4440fbfe3d687a40222a5d9cc238f841
mark <mark.trent86@gmail.com>
2019-11-04 20:25:28 +0200

    start stop

--- src/kill.sh
+++ src/kill.sh
@@ -5,3 +5,4 @@ ps -ef | grep az_farmer.py | grep -v grep | awk '{print $2}' | xargs kill
 ps -ef | grep az_crafting.py | grep -v grep | awk '{print $2}' | xargs kill
 ps -ef | grep az_gather.py | grep -v grep | awk '{print $2}' | xargs kill
 ps -ef | grep az_inv.py | grep -v grep | awk '{print $2}' | xargs kill
+ps -ef | grep az_race.py | grep -v grep | awk '{print $2}' | xargs kill
